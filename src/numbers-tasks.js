bb44127c53dd746b23b14e3c9d70c3facf611699
Zhuzhanchini <asot2006@gmail.com>
2023-11-23 16:23:49 +0300

    feat: implement getDistance function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -68,8 +68,10 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const point1 = x2 - x1;
+  const point2 = y2 - y1;
+  return Math.hypot(point1, point2);
 }
 
 /**
